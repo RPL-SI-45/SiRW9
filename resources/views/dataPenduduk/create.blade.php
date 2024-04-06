168f88291b445b2afed7f6fbc28a0c79ac099d1e
btarisl <btarisl@student.telkomuniversity.ac.id>
2024-04-06 21:18:08 +0700

    SIB-13
    Bootstrap done

--- resources/views/dataPenduduk/create.blade.php
+++ resources/views/dataPenduduk/create.blade.php
@@ -1,47 +1,69 @@
-<div class="container">
-    <br>
-    <h2>Masukkan Data Peminjaman Ruangan</h2>
-    <form action="/admin/data-penduduk/store" method="POST">
-        @csrf
-        <div class="mb-3">
-            <label>Nama Warga</label>
-            <input name="Nama_Warga" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        </div>
-        <div class="mb-3">
-            <label>NIK</label>
-            <input name="NIK" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        </div>
-        <div class="mb-3">
-            <label>Tanggal Lahir</label>
-            <input name="Tanggal_Lahir" type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        </div>
-        <select class="form-select" name="Jenis_Kelamin">
-            <option value="">Pilih Jenis Kelamin</option>
-            <option value="Pria">Pria</option>
-            <option value="Wanita">Wanita</option>
-        </select>
-        <div class="mb-3">
-            <label>Alamat</label>
-            <input name="Alamat" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        </div>
-        <select class="form-select" name="RT">
-            <option value="">Pilih RT</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-        </select>
-        <div>
-            <select class="form-select" name="Status_Perkawinan">
-                <option value="">Pilih Status Perkawinan</option>
-                <option value="Belum Kawin">Belum Kawin</option>
-                <option value="Kawin">Kawin</option>
-            </select>
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Pekerjaan</label>
-            <input name="Pekerjaan" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        </div>
-        <input type="submit" name="submit" value='Tambahkan'>
-    </form>
-</div>
+@extends('layouts.admin')
+@section('content')
+    <div class="pagetitle">
+      <h1>Tambahkan Data Penduduk</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="/admin/data-penduduk">Home</a></li>
+          <li class="breadcrumb-item">Data Penduduk</li>
+          <li class="breadcrumb-item active">Tambah</li>
+        </ol>
+      </nav>
+    </div>
+    <div class="card">
+        <div class="card-body">
+              <h5 class="card-title">Masukkan data penduduk baru</h5>
+                <form action="/admin/data-penduduk/store" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label>Nama Warga</label>
+                        <input name="Nama_Warga" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label>NIK</label>
+                        <input name="NIK" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label>Tanggal Lahir</label>
+                        <input name="Tanggal_Lahir" type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label>Jenis Kelamin</label>
+                        <select class="form-select" name="Jenis_Kelamin">
+                            <option value="">Pilih Jenis Kelamin</option>
+                            <option value="Pria">Pria</option>
+                            <option value="Wanita">Wanita</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label>Alamat</label>
+                        <input name="Alamat" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label>RT</label>
+                        <select class="form-select" name="RT">
+                            <option value="">Pilih RT</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label>Status Perkawinan</label>
+                        <select class="form-select" name="Status_Perkawinan">
+                            <option value="">Pilih Status Perkawinan</option>
+                            <option value="Belum Kawin">Belum Kawin</option>
+                            <option value="Kawin">Kawin</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Pekerjaan</label>
+                        <input name="Pekerjaan" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="text-center">
+                        <input type="submit" name="submit" class="btn btn-primary" value='Tambahkan'> 
+                    </div>
+                </form>
+            </div>
+@endsection
--- resources/views/dataPenduduk/index.blade.php
+++ resources/views/dataPenduduk/index.blade.php
@@ -1,35 +1,56 @@
-<a href="/admin/data-penduduk/create">Buat Peminjaman Ruangan Baru</a>
-<table border='1'>
-    <tr >
-        <th>ID</th>
-        <th>Nama</th>
-        <th>NIK</th>
-        <th>Tanggal Lahir</th>
-        <th>Jenis Kelamin</th>
-        <th>Alamat</th>
-        <th>RT</th>
-        <th>Status Perkawinan</th>
-        <th>Pekerjaan</th>
-    </tr>
-    @foreach($data_penduduk as $dp)
-        <tr>
-            <td>{{$dp->id}}</td>
-            <td>{{$dp->Nama_Warga}}</td>
-            <td>{{$dp->NIK}}</td>
-            <td>{{$dp->Tanggal_Lahir}}</td>
-            <td>{{$dp->Jenis_Kelamin}}</td>
-            <td>{{$dp->Alamat}}</td>
-            <td>{{$dp->RT}}</td>
-            <td>{{$dp->Status_Perkawinan}}</td>
-            <td>{{$dp->Pekerjaan}}</td>
-            <td style="display: flex; gap: 5px; justify-content:center">
-                <a href="/admin/data-penduduk/{{$dp->id}}/edit" class="btn btn-warning btn-sm">Edit ✏️</a>
-                <form action="/admin/data-penduduk/{{$dp->id}}" method="POST">
-                    @csrf
-                    @method('delete')
-                    <input class="btn btn-danger btn-sm" type="submit" value='Delete 🗑️' onclick="confirm('Hapus Data Peminjaman?')">
-                </form>
-            </td>
-        </tr>
-    @endforeach
-</table>
+@extends('layouts.admin')
+@section('content')
+    <div class="pagetitle" style="text-align: center;">
+      <h1>Tabel Data Penduduk</h1>
+    </div>
+    <section class="section">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title"></h5>
+              <a href="/admin/data-penduduk/create" class="btn btn-primary" class='card-title'>Tambah Data Penduduk</a>
+                    <table  class="table datatable">
+                        <thead>
+                            <tr >
+                                <th>ID</th>
+                                <th>Nama</th>
+                                <th>NIK</th>
+                                <th>Tanggal Lahir</th>
+                                <th>Jenis Kelamin</th>
+                                <th>Alamat</th>
+                                <th>RT</th>
+                                <th>Status Perkawinan</th>
+                                <th>Pekerjaan</th>
+                                <th> </th>
+                            </tr>
+                        </thead>
+                        @foreach($data_penduduk as $dp)
+                            <tr>
+                                <td>{{$dp->id}}</td>
+                                <td>{{$dp->Nama_Warga}}</td>
+                                <td>{{$dp->NIK}}</td>
+                                <td>{{$dp->Tanggal_Lahir}}</td>
+                                <td>{{$dp->Jenis_Kelamin}}</td>
+                                <td>{{$dp->Alamat}}</td>
+                                <td>{{$dp->RT}}</td>
+                                <td>{{$dp->Status_Perkawinan}}</td>
+                                <td>{{$dp->Pekerjaan}}</td>
+                                <td style="display: flex; gap: 5px; justify-content:center">
+                                    <a href="/admin/data-penduduk/{{$dp->id}}/edit" class="btn btn-warning btn-sm">Edit✏️</a>
+                                    <form action="/admin/data-penduduk/{{$dp->id}}" method="POST">
+                                        @csrf
+                                        @method('delete')
+                                        <input class="btn btn-danger btn-sm" type="submit" value='Delete🗑️' onclick="confirm('Hapus Data Peminjaman?')">
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </table>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+@endsection
