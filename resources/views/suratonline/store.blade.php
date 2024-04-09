2e25f95d297e029c3cd3c1b25df7d8ba35ce407c
gracemnd <graceamanda0404@gmail.com>
2024-04-09 12:09:53 +0700

    SIB-8
    store

--- resources/views/suratonline/store.blade.php
+++ resources/views/suratonline/store.blade.php
@@ -1,60 +1,7 @@
 @extends('layouts.guest')
 @section('content')
-      <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top header-inner-pages">
-    <div class="container d-flex align-items-center">
-
-      <h1 class="logo me-auto"><a href="index.html">SIRW9</a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
-      <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto " href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">Profil RW</a></li>
-          <li><a class="nav-link scrollto" href="#services">Berita Kegiatan</a></li>
-          <li class="dropdown"><a href="#"><span>Layanan</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-                  <li><a href="#">Panduan Layanan</a></li>
-                  <li><a href="#">Surat Online</a></li>
-                  <li><a href="#">Pembayaran Kas</a></li>
-                </ul>
-              </li>
-              <li class="dropdown"><a href="#"><span>Laporan</span> <i class="bi bi-chevron-down"></i></a>
-              <li><a href="#">Usulan</a></li>
-              <li><a href="#">Aduan</a></li>
-            </ul>
-          </li>
-          <li><a class="getstarted scrollto" href="#about">Login Admin</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-
-    </div>
-  </header><!-- End Header -->
-
-  <main id="main">
-
-    <!-- ======= Breadcrumbs ======= -->
-    <section id="breadcrumbs" class="breadcrumbs">
-      <div class="container">
-
-        <ol>
-          <li><a href="index.html">Home</a></li>
-        </ol>
-      </div>
-    </section><!-- End Breadcrumbs -->
-        <p>
-          
-        </p>
-      </div>
-    </section>
-
-  </main><!-- End #main -->
-
   <!-- ======= Footer ======= -->
   <footer id="footer">
-
     <div class="footer-newsletter">
       <div class="container">
         <div class="row justify-content-center">
@@ -74,27 +21,5 @@
               <strong>Email:</strong> KelompokE@gmail.com<br>
             </p>
           </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Services</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Surat Online</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Pembayaran Kas</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Usulan</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Aduan</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Berita Kegiatan</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Panduan Layanan</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Profil RW</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Website SiRW9</h4>
-            <p>Merupakan Website yang dibangun untuk membantu pendataan warga pada lingkungan Rukun Warga (RW)</p><br>
-            <p>Kecamatan Banyumanik, Kelurahan Banyumanik, Semarang</p>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
 @endsection
