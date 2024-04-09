c5df0ad75ba7850d7dab46518747f51a829729bb
gracemnd <graceamanda0404@gmail.com>
2024-04-09 12:26:44 +0700

    create

--- resources/views/suratonline/create.blade.php
+++ resources/views/suratonline/create.blade.php
@@ -17,19 +17,19 @@
                     @csrf
                     <div class="mb-3">
                         <label>Nama Lengkap</label>
-                        <input name="Nama_Lengkap" type="text" class="form-control" id="Nama_Lengkap" aria-describedby="Nama Lengkap Anda">
+                        <input name="nama_lengkap" type="text" class="form-control" id="nama_Lengkap" aria-describedby="Nama Lengkap Anda">
                     </div>
-                    <div class="col-md-10 mb-3">
-                        <label>Tanggal Lahir</label>
-                        <input name="Tanggal_Lahir" type="date" class="form-control" id="Tanggal_Lahir" aria-describedby="Tanggal Lahir Tinggal Anda">
+                    <div class="mb-3">
+                        <label>NIK</label>
+                        <input name="nik" type="text" class="form-control" id="nik" aria-describedby="NIK Anda">
                     </div>
                     <div class="col-md-2 mb-3">
-                        <label>NIK</label>
-                        <input name="NIK" type="text" class="form-control" id="NIK" aria-describedby="NIK Anda">
+                        <label>Tanggal Lahir</label>
+                        <input name="tanggal_lahir" type="date" class="form-control" id="tanggal_lahir" aria-describedby="Tanggal Lahir Anda">
                     </div>
                     <div class="mb-3">
                         <label>Jenis Kelamin</label>
-                        <select class="form-select" name="Jenis_Kelamin">
+                        <select class="form-select" name="jenis_kelamin">
                             <option value="">Jenis Kelamin</option>
                             <option value="P">Perempuan</option>
                             <option value="L">Laki - Laki</option>
@@ -37,21 +37,21 @@
                     </div>
                     <div class="mb-3 col-md-6">
                         <label>Alamat</label>
-                        <input name="Alamat" type="text" class="form-control" id="Alamat" aria-describedby="Alamat Anda">
-                    </div>
-                    <div class="mb-3 col-md-6">
-                        <label>Pekerjaan</label>
-                        <input name="Pekerjaan" type="text" class="form-control" id="Pekerjaan" aria-describedby="Pekerjaan Anda">
+                        <input name="alamat" type="text" class="form-control" id="alamat" aria-describedby="Alamat Anda">
                     </div>
                     <div class="mb-3">
-                        <label for="inputNumber" class="col-sm-2 col-form-label">Berkas Pendukung</label>
+                        <label>Pekerjaan</label>
+                        <input name="pekerjaan" type="text" class="form-control" id="pekerjaan" aria-describedby="Pekerjaan Anda">
+                    </div>
+                    <div class="mb-3">
+                        <label for="inputNumber" class="col-sm-2 col-form-label">Dokumen</label>
                         <div class="col-sm-12">
-                            <input class="form-control" type="file" name="Berkas_Pendukung(Link_Drive)">
+                            <input class="form-control" type="file" name="dokumen">
                         </div>
                     </div>
                     <div class="mb-3 col-md-6">
                         <label>Keperluan</label>
-                        <input name="Keperluan" type="text" class="form-control" id="Keperluan" aria-describedby="Keperluan">
+                        <input name="keperluan" type="text" class="form-control" id="keperluan" aria-describedby="Keperluan">
                     </div>
                     <div class="text-center">
                         <input type="submit" name="submit" class="btn btn-primary" value='Buat Surat'> 
