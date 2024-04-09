23e92eaffe08e6119c9db4f02bbb1dbe9b3ed014
gracemnd <graceamanda0404@gmail.com>
2024-04-09 11:46:15 +0700

    store

--- resources/views/suratonline/store.blade.php
+++ resources/views/suratonline/store.blade.php
@@ -11,29 +11,21 @@
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="nav-link scrollto " href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link   scrollto" href="#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+          <li><a class="nav-link scrollto" href="#about">Profil RW</a></li>
+          <li><a class="nav-link scrollto" href="#services">Berita Kegiatan</a></li>
+          <li class="dropdown"><a href="#"><span>Layanan</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
+                  <li><a href="#">Panduan Layanan</a></li>
+                  <li><a href="#">Surat Online</a></li>
+                  <li><a href="#">Pembayaran Kas</a></li>
                 </ul>
               </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
+              <li class="dropdown"><a href="#"><span>Laporan</span> <i class="bi bi-chevron-down"></i></a>
+              <li><a href="#">Usulan</a></li>
+              <li><a href="#">Aduan</a></li>
             </ul>
           </li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
+          <li><a class="getstarted scrollto" href="#about">Login Admin</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
@@ -78,34 +70,30 @@
               Mohon menunggu berita selanjutnya <br>
               <i>Have a nice day </i><br>
               <br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
+              <strong>Phone:</strong> +62 812-1234-1234<br>
+              <strong>Email:</strong> KelompokE@gmail.com<br>
             </p>
           </div>
 
           <div class="col-lg-3 col-md-6 footer-links">
             <h4>Our Services</h4>
             <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Surat Online</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Pembayaran Kas</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Usulan</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Aduan</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Berita Kegiatan</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Panduan Layanan</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Profil RW</a></li>
             </ul>
           </div>
 
           <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Social Networks</h4>
-            
-            <div class="social-links mt-3">
-              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            <h4>Website SiRW9</h4>
+            <p>Merupakan Website yang dibangun untuk membantu pendataan warga pada lingkungan Rukun Warga (RW)</p><br>
+            <p>Kecamatan Banyumanik, Kelurahan Banyumanik, Semarang</p>
             </div>
           </div>
-
         </div>
       </div>
     </div>
