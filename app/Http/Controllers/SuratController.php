a342a925cb7d4afbd6e4fc918b1648f303c046ea
Shuweck <arafiano.haputra@gmail.com>
2024-04-15 19:54:43 +0700

    Update SuratController.php

--- app/Http/Controllers/SuratController.php
+++ app/Http/Controllers/SuratController.php
@@ -102,44 +102,6 @@ class SuratController extends Controller
     ]);
     return redirect('/admin/suratonline');
 }
-//     public function update($id, Request $request){
-//         $suratonline = Surat_online::find($id);
-//         $request->validate([
-//             'dokumen' => 'mimes:png,jpg,jpeg,webp,pdf,docx'
-//         ]);
-
-//         if ($request->has('dokumen')) {
-
-//             $file = $request->file('dokumen');
-//             $extension = $file->getClientOriginalExtension();
-
-//             $filename = time().'.'.$extension;
-
-//             $path = 'uploads/suratonline/';
-
-//             if($suratonline->dokumen && File::exists(public_path($suratonline->dokumen))) {
-//                 File::delete(public_path($suratonline->dokumen));
-//             }
-
-//             $file->move($path, $filename);
-//             $suratonline->update(['dokumen' => $path.$filename]);
-//         }
-
-//         $suratonline->update([
-//             'nama_lengkap' => $request->nama_lengkap,
-//             'nik' => $request->nik,
-//             'keperluan' => $request->keperluan,
-//             'jenis_surat' => $request->jenis_surat,
-//             'tanggal_lahir' => $request->tanggal_lahir,
-//             'umur' => $request->umur,
-//             'status' => $request->status,
-//             'agama' => $request->agama,
-//             'jenis_kelamin' => $request->jenis_kelamin,
-//             'pekerjaan' => $request->pekerjaan,
-//             'status_surat' => $request->status_surat
-//         ]);
-//         return redirect('/admin/suratonline');
-// }
     public function destroy($id){
         $surat_online = Surat_online::find($id);
         $surat_online->delete();
