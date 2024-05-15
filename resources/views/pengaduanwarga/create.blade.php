6c58d6e7f00b51c43983c03b35a9082117a846d4
gracemnd <graceamanda0404@gmail.com>
2024-05-15 21:48:17 +0700

    SIB-19

--- resources/views/pengaduanwarga/create.blade.php
+++ resources/views/pengaduanwarga/create.blade.php
@@ -1,30 +1,30 @@
 @extends('layouts.admin')
 @section('content')
     <div class="pagetitle">
-      <h1>Tambah Data Usulan Warga</h1>
+      <h1>Tambah Aduan Warga</h1>
       <nav>
         <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="/admin/usulanwarga">Home</a></li>
-          <li class="breadcrumb-item active">Usulan Warga</li>
-          <li class="breadcrumb-item active">Tambah Usulan Warga</li>
+          <li class="breadcrumb-item"><a href="/admin/pengaduanwarga">Home</a></li>
+          <li class="breadcrumb-item active">Aduan Warga</li>
+          <li class="breadcrumb-item active">Tambah Aduan Warga</li>
         </ol>
       </nav>
     </div>
     <div class="card">
         <div class="card-body">
-              <h5 class="card-title">Masukkan usulan warga</h5>
-                <form id='usulanWargaForm' class="row g-3" action="/admin/usulanwarga/store" method="POST">
+              <h5 class="card-title">Masukkan Aduan warga</h5>
+                <form id='usulanWargaForm' class="row g-3" action="/admin/pengaduanwarga/store" method="POST">
                     @csrf
                     <div class="mb-3">
-                        <label>Judul Usulan</label>
-                        <input name="Judul_Usulan" type="text" class="form-control" id="Judul_Usulan" required>
+                        <label>Aduan</label>
+                        <input name="Aduan" type="text" class="form-control" id="Aduan" required>
                     </div>
                     <div class="col-md-8 mb-3">
-                        <label>Nama Pengusul</label>
-                        <input name="Nama_Pengusul" type="text" class="form-control" id="Nama_Pengusul" required>
+                        <label>Nama Pengadu</label>
+                        <input name="Nama_Pengadu" type="text" class="form-control" id="Nama_Pengadu" required>
                     </div>
                     <div class="col-md-4 mb-3">
-                        <label>RT</label>
+                        <label>RT Pengadu</label>
                         <select class="form-select" name="RT"  required>
                             <option value="">Pilih RT</option>
                             <option value="1">1</option>
@@ -33,19 +33,26 @@
                             <option value="4">4</option>
                         </select>
                     </div>
+                    <div class="col-md-4 mb-3">
+                        <label>Status Aduan</label>
+                        <select class="form-select" name="StatusAduan"  required>
+                            <option value="">Pilih Status</option>
+                            <option value="BelumDiverifikasi">Belum Diverifikasi</option>
+                            <option value="Terverifikasi">Terverifikasi</option>
+                        </select>
                     <div class="col-12">
                         <div class="form-floating">
-                        <textarea class="form-control" name="Detail_Usulan" placeholder="Detail_Usulan" id="Detail_Usulan" style="height: 100px;"></textarea>
-                        <label>Detail Usulan</label>
+                        <textarea class="form-control" name="Bukti_Aduan" placeholder="Bukti_Aduan" id="Bukti_Aduan" style="height: 100px;"></textarea>
+                        <label>Bukti Aduan</label>
                     </div>
                     </div>
                     <div class="text-center">
-                        <input type="submit" name="submit" class="btn btn-primary" value='Tambah Usulan'> 
+                        <input type="submit" name="submit" class="btn btn-primary" value='Tambah Aduan'> 
                     </div>
                 </form>
             </div>
             <script>
-                document.getElementById("usulanWargaform").addEventListener("submit", function(event) {
+                document.getElementById("pengaduanWargaform").addEventListener("submit", function(event) {
                     var form = event.target;
                     if (!form.checkValidity()) {
                         event.preventDefault();
